c3d09b20aec01bac846e94a25e48b2ea28e365f2
Jennifer Adams <jennadamscodes@gmail.com>
2023-10-08 14:52:43 -0400

    Move error message text to a resource file

--- DistractScript/Interpreter.cs
+++ DistractScript/Interpreter.cs
@@ -5,12 +5,6 @@ namespace DistractScript
 {
     public static class Interpreter
     {
-        public struct ErrorMessages
-        {
-            public static string MissingFilename = "You forgot the filename. It's okay, DistractScript forgets things, too.";
-            public static string TooManyFilenames = "Sorry, DistractScript can't multitask. Enter exactly one filename.";
-        }
-
         public static void Main(string[] args)
         {
             try
@@ -27,11 +21,11 @@ namespace DistractScript
         {
             if (args.Length == 0)
             {
-                throw new Exception(ErrorMessages.MissingFilename);
+                throw new Exception(Resources.ErrorMessages.MissingFilename);
             }
             else if (args.Length > 1)
             {
-                throw new Exception(ErrorMessages.TooManyFilenames);
+                throw new Exception(Resources.ErrorMessages.TooManyFilenames);
             }
         }
 
