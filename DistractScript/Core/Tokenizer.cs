01cb5a431146a30feabebb58fed527b7dd10774f
Jennifer Adams <jennadamscodes@gmail.com>
2023-10-08 23:33:22 -0400

    Implement TypeToken class

--- DistractScript/Core/Tokenizer.cs
+++ DistractScript/Core/Tokenizer.cs
@@ -7,10 +7,21 @@ namespace DistractScript.Core
 {
     public static class Tokenizer
     {
+        public static string[] Types = { "infodump", "george", "round", "pointy" };
+
         public static List<Token> GenerateTokens(string text)
         {
             var splitText = text.Split((char[])null, StringSplitOptions.RemoveEmptyEntries).ToList();
             var tokens = new List<Token>();
+
+            foreach (var substring in splitText)
+            {
+                if (Types.Contains(substring))
+                {
+                    var token = new TypeToken(substring);
+                    tokens.Add(token);
+                }
+            }
 
             return tokens;
         }
---
+++ DistractScript/Tokens/TypeToken.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DistractScript.Tokens
+{
+    public enum CustomType
+    {
+        StringType,
+        BoolType,
+        IntType,
+        DecimalType
+    }
+
+    class TypeToken : Token
+    {
+        public CustomType Type { get; private set; }
+
+        public TypeToken(string value)
+            : base(value)
+        {
+            switch (value)
+            {
+                case "infodump":
+                    Type = CustomType.StringType;
+                    break;
+                case "george":
+                    Type = CustomType.BoolType;
+                    break;
+                case "round":
+                    Type = CustomType.IntType;
+                    break;
+                case "pointy":
+                    Type = CustomType.DecimalType;
+                    break;
+            }
+        }
+    }
+}
