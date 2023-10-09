2e5113b17e6fbbcc13711437cab1b0339b6c455a
Jennifer Adams <jennadamscodes@gmail.com>
2023-10-08 23:27:15 -0400

    Create base Token class

---
+++ DistractScript/Tokens/Token.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DistractScript.Tokens
+{
+    public class Token
+    {
+        public string StringValue { get; private set; }
+
+        public Token(string value)
+        {
+            StringValue = value;
+        }
+    }
+}
