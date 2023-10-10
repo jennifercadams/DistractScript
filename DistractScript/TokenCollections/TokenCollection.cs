6d1875da59e7b66d687bb72465ae21844cfb81cf
Jennifer Adams <jennadamscodes@gmail.com>
2023-10-10 19:04:09 -0400

    Move Contains method to base TokenCollection class

---
+++ DistractScript/TokenCollections/TokenCollection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace DistractScript.TokenCollections
+{
+    public abstract class TokenCollection<T>
+    {
+        public static bool Contains(string tokenString)
+        {
+            var type = typeof(T);
+            var fields = type.GetFields().Where(f => f.FieldType == typeof(string));
+            foreach (var field in fields)
+            {
+                var fieldValue = field.GetValue(null).ToString();
+                if (fieldValue == tokenString)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
--- DistractScript/TokenCollections/TypeCollection.cs
+++ DistractScript/TokenCollections/TypeCollection.cs
@@ -1,29 +1,12 @@
 ﻿using System;
-using System.Linq;
 
 namespace DistractScript.TokenCollections
 {
-    public static class TypeCollection
+    public class TypeCollection : TokenCollection<TypeCollection>
     {
         public const string StringType = "infodump";
         public const string BoolType = "george";
         public const string IntType = "round";
         public const string DecimalType = "pointy";
-
-        public static bool Contains(string tokenString)
-        {
-            var type = typeof(TypeCollection);
-            var fields = type.GetFields().Where(f => f.FieldType == typeof(string));
-            foreach (var field in fields)
-            {
-                var fieldValue = field.GetValue(null).ToString();
-                if (fieldValue == tokenString)
-                {
-                    return true;
-                }
-            }
-
-            return false;
-        }
     }
 }
