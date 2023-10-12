01c19539cdf501d43b38a27774308689ed426da8
Jennifer Adams <jennadamscodes@gmail.com>
2023-10-11 21:33:34 -0400

    Implement custom Split method

--- DistractScript/Core/Tokenizer.cs
+++ DistractScript/Core/Tokenizer.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using DistractScript.Tokens;
 using DistractScript.Collections.TokenCollections;
 
@@ -10,7 +9,7 @@ namespace DistractScript.Core
     {
         public static List<Token> GenerateTokens(string text)
         {
-            var splitText = text.Split((char[])null, StringSplitOptions.RemoveEmptyEntries).ToList();
+            var splitText = Split(text);
             var tokens = new List<Token>();
 
             foreach (var substring in splitText)
@@ -38,6 +37,49 @@ namespace DistractScript.Core
             }
 
             return tokens;
+        }
+
+        private static List<string> Split(string text)
+        {
+            var splitText = new List<string>();
+
+            string token = "";
+            for (var i = 0; i < text.Length; i++)
+            {
+                char c = text[i];
+                if (char.IsWhiteSpace(c) && token == "")
+                {
+                    continue;
+                }
+                else if (char.IsWhiteSpace(c) && token != "")
+                {
+                    splitText.Add(token);
+                    token = "";
+                }
+                else if (c == '"')
+                {
+                    var endIndex = text.IndexOf('"', i + 1);
+                    var stringLiteral = text.Substring(i, endIndex - i + 1);
+                    splitText.Add(stringLiteral);
+                    i = endIndex;
+                    token = "";
+                }
+                else if (c == ';')
+                {
+                    if (token != "")
+                    {
+                        splitText.Add(token);
+                        token = "";
+                    }
+                    splitText.Add(c.ToString());
+                }
+                else
+                {
+                    token += c;
+                }
+            }
+
+            return splitText;
         }
     }
 }
