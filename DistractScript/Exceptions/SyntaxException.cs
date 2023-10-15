9114b8ea9a57556f014a77071023c6be3d702fc6
Jennifer Adams <jennadamscodes@gmail.com>
2023-10-14 23:25:39 -0400

    Tweak error message in SyntaxException

--- DistractScript/Exceptions/SyntaxException.cs
+++ DistractScript/Exceptions/SyntaxException.cs
@@ -4,7 +4,7 @@ namespace DistractScript.Exceptions
 {
     public class SyntaxException : Exception
     {
-        public const string ActualWithExpected = "Unexpected '{0}'; expected '{1}'. Line {2}:{3}";
+        public const string ActualWithExpected = "Unexpected '{0}'; expected {1}. Line {2}:{3}";
         public const string ActualOnly = "Unexpected '{0}'. Line {1}:{2}";
 
         public SyntaxException(string actual, string expected, int line, int col)
