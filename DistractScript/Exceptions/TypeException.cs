fd42fd2878e7f31e145a1b138fdfb7dc0f32ae9f
Jennifer Adams <jennadamscodes@gmail.com>
2023-10-14 23:26:24 -0400

    Add TypeException exception class

---
+++ DistractScript/Exceptions/TypeException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DistractScript.Exceptions
+{
+    public class TypeException : Exception
+    {
+        public const string InvalidType = "Value of invalid type assigned to {0}: expected {1}, actual {2}. Line {3}:{4}";
+
+        public TypeException(string variableName, string expected, string actual, int line, int column)
+            : base(String.Format(InvalidType, variableName, expected, actual, line, column))
+        { }
+    }
+}
