e24c00e15f81dd8c3d629b510f99f3b28c1fdf9a
Jennifer Adams <jennadamscodes@gmail.com>
2023-10-15 14:55:15 -0400

    Implement generic Variable class

---
+++ DistractScript/Variables/Variable.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DistractScript.Variables
+{
+    public class Variable<T>
+    {
+        public string Name { get; private set; }
+        private T Value { get; set; }
+
+        public Variable(string name) 
+        {
+            Name = name;
+            Value = default(T);
+        }
+
+        public Variable(string name, T value)
+        {
+            Name = name;
+            Value = value;
+        }
+
+        public T Get()
+        {
+            return Value;
+        }
+
+        public void Set(T newValue)
+        {
+            Value = newValue;
+        }
+    }
+}
