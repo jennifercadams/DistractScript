44b377ffc037aac213d87048f3b841d238cea17d
Jennifer Adams <jennadamscodes@gmail.com>
2023-10-17 20:00:38 -0400

    Create VarAlreadyExistsException

---
+++ DistractScript/Exceptions/VarAlreadyExistsException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DistractScript.Exceptions
+{
+    public class VarAlreadyExistsException : Exception
+    {
+        private const string VarAlreadyExists = "A variable named '{0}' is already defined in this scope";
+
+        public VarAlreadyExistsException(string name)
+            : base(String.Format(VarAlreadyExists, name)) { }
+    }
+}
