247542f56d5a482d625581be8d422a268b37c903
Jennifer Adams <jennadamscodes@gmail.com>
2023-10-17 20:46:52 -0400

    Create VarDoesNotExistException

---
+++ DistractScript/Exceptions/VarDoesNotExistException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DistractScript.Exceptions
+{
+    public class VarDoesNotExistException : Exception
+    {
+        public const string VarDoesNotExist = "The variable '{0}' does not exist in this scope";
+
+        public VarDoesNotExistException(string name)
+            : base(String.Format(VarDoesNotExist, name)) { }
+    }
+}
