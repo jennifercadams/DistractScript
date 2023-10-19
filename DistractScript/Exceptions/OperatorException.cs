adce9bc73ebc22984e83e9897af14a6a4c07eeda
Jennifer Adams <jennadamscodes@gmail.com>
2023-10-19 19:30:08 -0400

    Create OperatorException

---
+++ DistractScript/Exceptions/OperatorException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DistractScript.Exceptions
+{
+    public class OperatorException : Exception
+    {
+        private const string InvalidOperator = "Operator {0} cannot be used with type {1}";
+
+        public OperatorException(string operatorString, string typeString)
+            : base(String.Format(InvalidOperator, operatorString, typeString)) { }
+    }
+}
