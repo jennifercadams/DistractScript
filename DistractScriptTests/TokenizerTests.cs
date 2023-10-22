dac53871138a8d3e2754efd458fb4fcb0da4ec8e
Jennifer Adams <jennadamscodes@gmail.com>
2023-10-22 14:07:55 -0400

    Add operator token test cases

--- DistractScriptTests/TokenizerTests.cs
+++ DistractScriptTests/TokenizerTests.cs
@@ -17,6 +17,54 @@ namespace DistractScriptTests
             VerifyKeywordToken(stringValue, keyword);
         }
 
+        [TestMethod]
+        public void GenerateTokens_Operator_Assignment()
+        {
+            var stringValue = OperatorCollection.Assignment;
+            var operatorType = Operator.Assignment;
+            VerifyOperatorToken(stringValue, operatorType);
+        }
+
+        [TestMethod]
+        public void GenerateTokens_Operator_Sum()
+        {
+            var stringValue = OperatorCollection.Sum;
+            var operatorType = Operator.Sum;
+            VerifyOperatorToken(stringValue, operatorType);
+        }
+
+        [TestMethod]
+        public void GenerateTokens_Operator_Difference()
+        {
+            var stringValue = OperatorCollection.Difference;
+            var operatorType = Operator.Difference;
+            VerifyOperatorToken(stringValue, operatorType);
+        }
+
+        [TestMethod]
+        public void GenerateTokens_Operator_Product()
+        {
+            var stringValue = OperatorCollection.Product;
+            var operatorType = Operator.Product;
+            VerifyOperatorToken(stringValue, operatorType);
+        }
+
+        [TestMethod]
+        public void GenerateTokens_Operator_Quotient()
+        {
+            var stringValue = OperatorCollection.Quotient;
+            var operatorType = Operator.Quotient;
+            VerifyOperatorToken(stringValue, operatorType);
+        }
+
+        [TestMethod]
+        public void GenerateTokens_Operator_Modulo()
+        {
+            var stringValue = OperatorCollection.Modulo;
+            var operatorType = Operator.Modulo;
+            VerifyOperatorToken(stringValue, operatorType);
+        }
+
         private void VerifyKeywordToken(string stringValue, Keyword keyword)
         {
             var tokens = Tokenizer.GenerateTokens(stringValue);
@@ -27,6 +75,18 @@ namespace DistractScriptTests
             var keywordToken = tokens[0] as KeywordToken;
             Assert.IsTrue(keywordToken.StringValue == stringValue);
             Assert.IsTrue(keywordToken.Keyword == keyword);
+        }
+
+        private void VerifyOperatorToken(string stringValue, Operator operatorType)
+        {
+            var tokens = Tokenizer.GenerateTokens(stringValue);
+
+            Assert.IsTrue(tokens.Count == 1);
+            Assert.IsTrue(tokens[0] is OperatorToken);
+
+            var operatorToken = tokens[0] as OperatorToken;
+            Assert.IsTrue(operatorToken.StringValue == stringValue);
+            Assert.IsTrue(operatorToken.Operator == operatorType);
         }
 
     }
