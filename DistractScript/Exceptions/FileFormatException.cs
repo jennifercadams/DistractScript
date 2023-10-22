174b22e833188a79bd0d061d20a5e64350838315
Jennifer Adams <jennadamscodes@gmail.com>
2023-10-21 21:36:31 -0400

    Add missing public access modifier to FileFormatException

--- DistractScript/Exceptions/FileFormatException.cs
+++ DistractScript/Exceptions/FileFormatException.cs
@@ -2,7 +2,7 @@
 
 namespace DistractScript.Exceptions
 {
-    class FileFormatException : Exception
+    public class FileFormatException : Exception
     {
         private const string IncorrectFileFormat = "Incorrect file format. DistractScript expects text files with the extension \".adhd\".";
 
