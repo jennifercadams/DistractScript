f033bbb470486e6dcb1886627315cc93d00a9e3a
Jennifer Adams <jennadamscodes@gmail.com>
2023-10-22 14:04:16 -0400

    Add DeclareVar keyword token test case

--- DistractScriptTests/TokenizerTests.cs
+++ DistractScriptTests/TokenizerTests.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using DistractScript.Core;
+using DistractScript.Tokens;
+using DistractScript.Tokens.TokenCollections;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace DistractScriptTests
@@ -6,6 +9,25 @@ namespace DistractScriptTests
     [TestClass]
     public class TokenizerTests
     {
+        [TestMethod]
+        public void GenerateTokens_Keyword_DeclareVar()
+        {
+            var stringValue = KeywordCollection.DeclareVar;
+            var keyword = Keyword.DeclareVar;
+            VerifyKeywordToken(stringValue, keyword);
+        }
+
+        private void VerifyKeywordToken(string stringValue, Keyword keyword)
+        {
+            var tokens = Tokenizer.GenerateTokens(stringValue);
+
+            Assert.IsTrue(tokens.Count == 1);
+            Assert.IsTrue(tokens[0] is KeywordToken);
+
+            var keywordToken = tokens[0] as KeywordToken;
+            Assert.IsTrue(keywordToken.StringValue == stringValue);
+            Assert.IsTrue(keywordToken.Keyword == keyword);
+        }
 
     }
 }
